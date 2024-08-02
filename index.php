db7b11456019f9937de05b7a609b58fa20d0b375
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-08-02 16:38:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Nida info.</title>
-    <link href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nida Info</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
-    <form action="nida.php" method="post">
-        <input type="text" name="nida" id="nida" placeholder="nida yako hapa.">
-        <br>
-        <button type="submit">check</button>
-        
-    </form>
+        <form action="nida.php" method="post" class="form">
+            <h2>Enter Your NIDA ID</h2>
+            <input type="text" name="nida" id="nida" placeholder="NIDA yako hapa" required>
+            <button type="submit">Check</button>
+        </form>
     </div>
     <footer>
-          <p>Dev By Sam Ochu</p>
+        <p>Dev By Sam Ochu</p>
     </footer>
 </body>
 </html>
